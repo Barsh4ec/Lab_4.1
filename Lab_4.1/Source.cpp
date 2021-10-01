40754ae2cc0946c8b5639f6896af32e4bbf334bc
Barsh4ec <tarasborshch4@gmail.com>
2021-10-01 20:19:32 +0300

    first commit

---
+++ Lab_4.1/Source.cpp
@@ -0,0 +1,65 @@
+// Lab_04_1.cpp
+// Борща Тараса
+// Лабораторна робота № 4.1
+// Цикли
+// Варіант 2
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int k, N;
+	double S;
+
+	cout << " k= "; cin >> k;
+	cout << " N= "; cin >> N;
+
+	// while (...) {...}
+
+	S = 0;
+	k = N;
+	while (k <= 19)
+	{
+		S += sqrt((sin(k) * sin(k)) + (cos(N / k) * cos(N / k)));
+		k++;
+	}
+
+	cout << S << endl;
+
+	//do{...} while(...)
+
+	S = 0;
+	k = N;
+	do
+	{
+		S += sqrt((sin(k) * sin(k)) + (cos(N / k) * cos(N / k)));
+		k++;
+	} while (k <= 19);
+
+	cout << S << endl;
+
+	//for(...;...;n++) {...}
+
+	S = 0;
+	for (k = N; k <= 19; k++)
+	{
+		S += sqrt((sin(k) * sin(k)) + (cos(N / k) * cos(N / k)));
+	}
+
+	cout << S << endl;
+
+	//for(...;...;n--) {...}
+
+	S = 0;
+	for (k = 19; k >= N; k--)
+	{
+		S += sqrt((sin(k) * sin(k)) + (cos(N / k) * cos(N / k)));
+	}
+
+	cout << S << endl;
+
+	return 0;
+}
